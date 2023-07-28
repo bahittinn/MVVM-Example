aa0c494f083b085851aa161e1036aaa8e47a8137
bahittinn <bahittin84@gmail.com>
2023-07-28 21:47:03 +0300

    last changes

--- MVVM-Example/View/ViewController.swift
+++ MVVM-Example/View/ViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var usernameLabel: UILabel!
+        @IBOutlet weak var usernameLabel: UILabel!
         @IBOutlet weak var emailLabel: UILabel!
 
         var viewModel: UserViewModel!
